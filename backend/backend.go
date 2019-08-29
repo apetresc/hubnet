82f61ca49e34723e8d62e3b17bf147865527aadf
Adrian Petrescu <adrian@apetre.sc>
2019-08-29 16:25:40 -0400

    Actually populate the group list with real groups

--- backend/backend.go
+++ backend/backend.go
@@ -2,6 +2,8 @@ package backend
 
 import (
 	"database/sql"
+	"fmt"
+	"log"
 	"sync"
 
 	nntp "github.com/dustin/go-nntp"
@@ -32,31 +34,48 @@ func (sb *SQLBackend) GetArticle(group *nntp.Group, id string) (*nntp.Article, e
 
 func (sb *SQLBackend) GetArticles(group *nntp.Group, from, to int64) ([]nntpserver.NumberedArticle, error) {
 	rv := make([]nntpserver.NumberedArticle, 0, 0)
-	rv = append(rv, nntpserver.NumberedArticle{})
+	//rv = append(rv, nntpserver.NumberedArticle{})
 	return rv, nil
 }
 
 func (sb *SQLBackend) GetGroup(name string) (*nntp.Group, error) {
+	row := sb.DB.QueryRow("SELECT name, type FROM groups WHERE name=\"" + name + "\"")
+	var _type string
+	if err := row.Scan(&name, &_type); err != nil {
+		log.Fatal(err)
+	}
 	return &nntp.Group{
-		Name:        "rec.games.awesome",
-		Description: "A cool test server",
-		Count:       2,
-		Low:         2,
-		High:        3,
+		Name:        name,
+		Description: fmt.Sprintf("%ss for the Github repository at ", _type),
+		Count:       0,
+		Low:         0,
+		High:        0,
 		Posting:     nntp.PostingNotPermitted,
 	}, nil
 }
 
 func (sb *SQLBackend) ListGroups(max int) ([]*nntp.Group, error) {
 	rv := make([]*nntp.Group, 0, 0)
-	rv = append(rv, &nntp.Group{
-		Name:        "rec.games.awesome",
-		Description: "A cool test server",
-		Count:       2,
-		Low:         2,
-		High:        3,
-		Posting:     nntp.PostingNotPermitted,
-	})
+	rows, err := sb.DB.Query("SELECT name, type FROM groups")
+	defer rows.Close()
+	if err != nil {
+		log.Fatalf("Error listing groups: %v", err)
+	}
+	for rows.Next() {
+		var name string
+		var _type string
+		if err := rows.Scan(&name, &_type); err != nil {
+			log.Fatal(err)
+		}
+		rv = append(rv, &nntp.Group{
+			Name:        name,
+			Description: fmt.Sprintf("%ss for the Github repository at ", _type),
+			Count:       0,
+			Low:         0,
+			High:        0,
+			Posting:     nntp.PostingNotPermitted,
+		})
+	}
 	return rv, nil
 }
 
